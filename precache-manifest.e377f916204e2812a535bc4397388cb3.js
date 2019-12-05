3548ba49c60163543522e2db1ca3b40ef293f409
Alex Hodovas <o.hodovas@gmail.com>
2019-12-05 12:38:05 +0200

    Updates

---
+++ precache-manifest.e377f916204e2812a535bc4397388cb3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6814b9861de5774b5761d8212469b8b4",
+    "url": "/react_static-list-of-posts/index.html"
+  },
+  {
+    "revision": "c3245031cf9acbb6d59d",
+    "url": "/react_static-list-of-posts/static/css/main.d0ee8644.chunk.css"
+  },
+  {
+    "revision": "d1bf606ea68b30240b50",
+    "url": "/react_static-list-of-posts/static/js/2.a7975bab.chunk.js"
+  },
+  {
+    "revision": "c3245031cf9acbb6d59d",
+    "url": "/react_static-list-of-posts/static/js/main.13bb8d55.chunk.js"
+  },
+  {
+    "revision": "3fdea9cdc8e2a1e9a77e",
+    "url": "/react_static-list-of-posts/static/js/runtime~main.c48bb6e1.js"
+  }
+]);
